89c23de5f7271cd3208e78b502c5a96537b15cad
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2025-08-09 17:01:26 -0700

    Initial Commit

---
+++ NightlyListMakerTests/NightlyListMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightlyListMakerTests.swift
+//  NightlyListMakerTests
+//
+//  Created by Tejas Patel on 8/9/25.
+//
+
+import Testing
+@testable import NightlyListMaker
+
+struct NightlyListMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
